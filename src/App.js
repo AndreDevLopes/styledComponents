931d87b9d6e2a78cea136f52a1e1f637f95b66d5
unknown <andrelopessfla@gmail.com>
2020-06-30 16:16:48 -0300

    novo style para o span

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import {Title} from './style';
 function App() {
   return (
     <div>
-      <Title>Hello Word</Title>
+      <Title>Hello Word
+        <span>menor</span>
+      </Title>
     </div>
   );
 }
